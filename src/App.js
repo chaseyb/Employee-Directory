ac9cea36fe9e407c328b7fc31778f23cd0b9d5b4
Chase Johnson <Chase.b.johnson@gmail.com>
2020-08-18 18:31:51 -0700

    Updated file structure.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Header from './components/Header'
-import Directory from './components/Directory'
-import Footer from './components/Footer'
-import './App.css';
+import Main from './components/Main';
+import Wrapper from './components/Wrapper';
+import Header from './components/Header';
+import "./App.css";
 
 function App() {
-  return (<div className="App" >
-    <div className="container" > </div>
-    <Header />
-    <Directory />
-    <Footer />
-  </div>
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+    </div>
   );
 }
 
-export default App;
+export default App; 
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default {
-    // Gets all users from API
     getUsers: function () {
+        // Return Data from Randomuser API 
         return axios.get("https://randomuser.me/api/?results=200&nat=us");
     }
 };
